7160c43d0d1e278861f175dbbd419dae96702d39
Glebanister <glebmar2001@gmail.com>
2020-11-20 01:28:29 +0300

    Add script for separate diagram generation

--- generate-svg.sh
+++ generate-svg.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ];
-    then echo "Specify .dot file path as first parameter"
+if [ "$#" -ne 1 ]; then
+    echo "Specify directory with .dot files"
     exit
 fi
 
-dot -Tsvg -O $1
+path=$(realpath $1)
+svgPath="$path/svg"
+mkdir -p $svgPath
+
+for i in $(find $1 -name "*.dot" -type f); do
+    filename=$(echo $i | sed 's#.*/##')
+    dot $1/$filename -Tsvg -o $svgPath/$filename.svg
+done
