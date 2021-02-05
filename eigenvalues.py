08a4e9e372c9d2328161392f2b8ea66330dd2dcd
Glebanister <glebmar2001@gmail.com>
2021-02-05 22:15:25 +0300

    Add doc to main

--- eigenvalues.py
+++ eigenvalues.py
@@ -9,6 +9,18 @@ from scipy.sparse.csgraph import laplacian
 
 
 def main(args: [str]) -> None:
+    '''
+    Generate eigenvalue distribution chart from graph <input-file>.
+    The input format of the graph, which consists of directed edges
+    [(a_1, b_1), ... (a_n, b_n)], where a_i, b_i are 0-indexed integers:
+
+    a_1 b_1
+    a_2 b_2
+    ...
+    a_n b_n
+
+    The output will be generated in the <input-file>-eigval.png
+    '''
     input_filename = args[0]
     edges = []
     max_node = 0
@@ -20,13 +32,16 @@ def main(args: [str]) -> None:
     nodes = max_node + 1
     mtx = np.zeros(shape=(nodes, nodes))
     for f, t in edges:
-        mtx[f, t] = 1.0
+        mtx[f, t] += 1.0
+        mtx[t, f] += 1.0
     laplacian_mtx = laplacian(mtx)
+    print(f'Laplacian: \n{laplacian_mtx}')
     print(f'Nodes in graph: {nodes}')
     vals = eigvals(laplacian_mtx)
     print(f'Eigenvalues: {vals}')
     plt.plot(sorted(vals))
-    plt.show()
+    # plt.show()
+    # plt.savefig()
     plt.savefig(input_filename + '-eigval.svg')
 
 
